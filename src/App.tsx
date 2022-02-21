042d500afc430a02a0943de493e6956d7067cd7a
Philip Ziolkowski <philipz4848@gmail.com>
2022-02-20 18:33:27 -0800

    Updating README

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,25 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-        <h1 className='text-center'></h1>
+
         
     </div>
   );
 }
 
 export default App;
+
+// ### Rules
+
+// If the player wins, they gain 1 point. If the computer wins, the player loses one point.
+
+// - Scissors beats Paper
+// - Paper beats Rock
+// - Rock beats Lizard
+// - Lizard beats Spock
+// - Spock beats Scissors
+// - Scissors beats Lizard
+// - Paper beats Spock
+// - Rock beats Scissors
+// - Lizard beats Paper
+// - Spock beats Rock
