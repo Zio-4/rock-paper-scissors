149d3a32e823a4d42244a514fb67b29a4fa2ec3c
Philip Ziolkowski <philipz4848@gmail.com>
2022-02-21 23:04:31 -0800

    Adding and styling rules

--- src/Components/Gameboard.tsx
+++ src/Components/Gameboard.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import Pentagon from '../images/bg-pentagon.svg'
+import RulesPic from '../images/image-rules-bonus.svg'
+import CloseButton from '../images/icon-close.svg'
 
 function Gameboard() {
   return (
-    <img src={Pentagon} alt='Pentagon game board background' className='w-56 mx-auto mt-20'/>
+    <div>
+      <img src={Pentagon} alt='Pentagon game board background' className=' mx-auto mt-40 z-0'/>
+      
+      <div className='bg-white w-96 mx-auto rounded-lg'>
+        <div className='flex justify-between px-6'>
+          <p className='text-dark-text text-3xl pt-4'>Rules</p>
+          <img src={CloseButton} alt='close button' className='h-6 w-6 mt-6'/>
+        </div>
+        <img src={RulesPic} alt='image of rules' className='mx-auto z-10 mt-4 pb-10'/>
+      </div>
+    </div>
+   
   )
 }
 
--- src/Components/Rules.tsx
+++ src/Components/Rules.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {MouseEvent} from 'react'
 
 function Rules() {
+
+  const showRules = (e: React.MouseEvent<HTMLElement>) => {
+    
+  }
+
   return (
     <div className='flex justify-end mr-6 mt-20'>
-        <button className='text-white border-2 border-header-outline py-1 px-7 rounded-md'>Rules</button>
+        <button onClick={showRules} className='text-white border-2 border-header-outline py-1 px-7 rounded-md'>Rules</button>
     </div>
   )
 }
