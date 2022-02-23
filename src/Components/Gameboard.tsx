ece4aa18e90adb028e9db47dcca2c0deb654db3d
Philip Ziolkowski <philipz4848@gmail.com>
2022-02-22 21:25:46 -0800

    Adding functionality to rules button, showing rules on click

--- src/Components/Gameboard.tsx
+++ src/Components/Gameboard.tsx
@@ -1,13 +1,12 @@
-import React, {MouseEvent} from 'react'
+import React, {useState} from 'react'
 import Pentagon from '../images/bg-pentagon.svg'
 import RulesPic from '../images/image-rules-bonus.svg'
 import CloseButton from '../images/icon-close.svg'
 
 function Gameboard() {
+  const [modalState, setModalState] = useState(false)
 
-  const showRules = (e: React.MouseEvent<HTMLElement>) => {
-    
-  }
+  console.log(modalState)
 
   return (
     <div>
@@ -15,15 +14,22 @@ function Gameboard() {
       
       {/* hidden | view height? */}
       {/* On md+ viewport change close to be aligned with rules text */}
-      <div className='bg-white hidden fixed left-0 top-0 w-screen h-screen rounded-lg z-10 overflow-auto'>
+      {/* <div className={`bg-white fixed left-0 top-0 w-screen h-screen rounded-lg z-10 overflow-auto ${modalState}`}> */}
+      <div className={`bg-white fixed left-0 top-0 w-screen h-screen rounded-lg z-10 overflow-auto ${modalState ? null : 'hidden'}`}>
         <div className='flex flex-col px-6'>
           <p className='order-1 text-dark-text text-3xl pt-24 mx-auto'>Rules</p>
-          <img src={CloseButton} alt='close button' className='order-last h-6 w-6 mt-24 mx-auto'/>
+          <img src={CloseButton}
+           alt='close button' 
+           onClick={() => setModalState(!modalState)} 
+           className='order-last h-6 w-6 mt-24 mx-auto'/>
           <img src={RulesPic} alt='image of rules' className='order-2 mx-auto z-10 mt-24 pb-10'/>
         </div>
       </div>
       <div className='flex justify-end mr-6 mt-20'>
-        <button onClick={showRules} className='text-white border-2 border-header-outline py-1 px-7 rounded-md'>Rules</button>
+        <button 
+        onClick={() => setModalState(!modalState)} 
+        className='text-white border-2 border-header-outline py-1 px-7 rounded-md'
+        >Rules</button>
     </div>
     </div>
   )
