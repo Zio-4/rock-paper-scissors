c489a3fe43c8f5163f42265bb4e2fdacc25eba10
Philip Ziolkowski <philipz4848@gmail.com>
2022-03-04 00:30:21 -0800

    Aligning buttons correctly on mobile during game

--- src/Components/PlayingGame.tsx
+++ src/Components/PlayingGame.tsx
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react'
-import Scissors from '../images/icon-scissors.svg'
-import Lizard from '../images/icon-lizard.svg'
-import Paper from '../images/icon-paper.svg'
-import Rock from '../images/icon-rock.svg'
-import Spock from '../images/icon-spock.svg'
+import scissors from '../images/icon-scissors.svg'
+import lizard from '../images/icon-lizard.svg'
+import paper from '../images/icon-paper.svg'
+import rock from '../images/icon-rock.svg'
+import spock from '../images/icon-spock.svg'
 
 interface IplayerButton {
   playerButtonPick: string
 }
+
+
 
 function PlayingGame({playerButtonPick}: IplayerButton) {
   const [computerButtonPick, setComputerButtonPick] = useState<string>('')
@@ -22,21 +24,22 @@ function PlayingGame({playerButtonPick}: IplayerButton) {
     if (number === 3) setComputerButtonPick('rock')
     if (number === 4) setComputerButtonPick('spock')
     if (number === 5) setComputerButtonPick('lizard')
-
-
   }, [])
+
+
+
 
   const plays = () => {
       return (
         <div>
-          <div className='flex flex-wrap justify-center space-x-20 mt-20'>
+          <div className='flex justify-center space-x-10 mt-20 mb-12'>
             <div>
-              <div className=' mb-4'>
+              <div className='mb-4'>
                 <div className={`bg-${playerButtonPick}-bg-first -mb-[116px] rounded-full w-28 h-28 mx-auto`}></div>
                 <div className={`bg-${playerButtonPick}-bg-last -mb-24 rounded-full w-28 h-28 mx-auto`}></div>
                 <div className='bg-slate-300 -mb-[76px] rounded-full w-20 h-20 z-10 mx-auto'></div>
                 <div className='bg-white rounded-full -mb-16 w-20 h-20 z-20 mx-auto'></div>
-                <img src={Scissors} alt={`${playerButtonPick} button`} className='mx-auto w-9 z-30' />
+                <img src={scissors} alt={`${playerButtonPick} button`} className='mx-auto w-9 z-30' />
               </div>
             </div>
 
@@ -44,15 +47,18 @@ function PlayingGame({playerButtonPick}: IplayerButton) {
             <div className=''>
               <div className={`bg-computer-placeholder-button opacity-20 rounded-full w-28 h-28 ${!!computerButtonPick ? 'hidden' : null}`}></div>
             </div>
+            
+            <div className={`${!!computerButtonPick ? null : 'hidden'}`}>
+              <div className={`bg-${computerButtonPick}-bg-first -mb-[116px] rounded-full w-28 h-28 mx-auto`} ></div>
+              <div className={`bg-${computerButtonPick}-bg-last -mb-24 rounded-full w-28 h-28 mx-auto`}></div>
+              <div className='bg-slate-300 -mb-[76px] rounded-full w-20 h-20 z-10 mx-auto'></div>
+              <div className='bg-white rounded-full -mb-16 w-20 h-20 z-20 mx-auto' ></div>
+              <img src={lizard} alt={`${computerButtonPick} button`} className='mx-auto w-9 z-30'/>
+            </div>
+
           </div>
 
-          <div className={`${!!computerButtonPick ? null : 'hidden'}`}>
-            <div className={`bg-${computerButtonPick}-bg-first -mb-[116px] rounded-full w-28 h-28 mx-auto`} ></div>
-            <div className={`bg-${computerButtonPick}-bg-last -mb-24 rounded-full w-28 h-28 mx-auto`}></div>
-            <div className='bg-slate-300 -mb-[76px] rounded-full w-20 h-20 z-10 mx-auto'></div>
-            <div className='bg-white rounded-full -mb-16 w-20 h-20 z-20 mx-auto' ></div>
-            <img src={Scissors} alt={`${computerButtonPick} button`} className='mx-auto w-9 z-30'/>
-          </div>
+
 
           <div className='flex flex-wrap justify-between space-x-2 mt-5'>
               <div className='text-lg text-white uppercase tracking-widest ml-12'>you picked</div>
